0fd725751850530f7bf0427bdba240f35f05aa3c
Boloyyu90 <balaputra20@gmail.com>
2025-09-05 13:12:14 +0700

    remove: scoreOverride examQuestions

---
+++ prisma/migrations/20250905060706_drop_score_override/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `score_override` on the `exam_questions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "exam_questions" DROP COLUMN "score_override";
